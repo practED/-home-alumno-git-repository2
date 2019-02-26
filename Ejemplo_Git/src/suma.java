6ab27948e00c3d91d230d719cb065f5e96172405
alumno <alumno@alumno-1dam>
2019-02-26 14:05:24 +0100

    cambio cadena

--- Ejemplo_Git/src/suma.java
+++ Ejemplo_Git/src/suma.java
@@ -3,10 +3,10 @@ public class suma {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a=1;
-		int b=5;
-		int result=0;
-		result=a-b;
+		String a="b";
+		String b="a";
+		String result="";
+		result=a.concat(b);
 
 	}
 
